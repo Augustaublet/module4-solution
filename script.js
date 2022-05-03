c83b532c22775f9a587612c7c129282ab232af8c
August Aublet <august.aublet@gmail.com>
2022-05-03 20:10:35 +0200

    wrap code inside IIFE,Part 1,2 and 6

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+(function (){
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -62,3 +63,7 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
     // helloSpeaker.xxxx
   }
 }
+
+
+})();
+
